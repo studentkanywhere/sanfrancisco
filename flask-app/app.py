c0001e814cd53d5a55a5e2ae7d9fdde3af8311cc
Kru <studentk1000@gmail.com>
2025-06-17 15:53:47 +0530

    i added light mode and dark mode

--- flask-app/app.py
+++ flask-app/app.py
@@ -14,5 +14,27 @@ def profile():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':  
     app.run(debug=True)
+
+flask-app/
+  static/
+    1.jpg
+    2.jpg
+    3.jpg
+    4.jpg
+    5.jpg
+    6.jpg
+    7.jpg
+    8.jpg
+    9.jpg
+    10.jpg
+    templates/
+        pythonofsf.html
+        profile.html
+        login.html
+        signup.html
